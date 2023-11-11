e47f31ecb00a98ed8690881d818fdf1cf1e160dc
ColorfullMadness <podniebnyvader@gmail.com>
2023-11-11 15:11:38 +0100

    Added MAC address and UUID to config handler, changed /status/health to return 204

--- src/config_handler.rs
+++ src/config_handler.rs
@@ -1,20 +1,44 @@
+use std::fmt::{Display, Formatter, write};
 use serde::Serialize;
-use sysinfo::{System, SystemExt};
-
+use sysinfo::{System, SystemExt, NetworkExt, Networks, NetworksExt};
+use log::info;
+use uuid;
 #[derive(Serialize)]
 pub struct ConfigHandler {
     pub ip_address: String,
     pub platform: String,
     pub templates_loc: String,
+    pub mac_address: String,
+    pub uuid: String
+}
+
+impl Display for ConfigHandler{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write(f, format_args!("Config: ip_address: {}, mac_address: {}, uuid: {}",
+            self.ip_address,
+            self.mac_address,
+            self.uuid))
+    }
 }
 
 impl Default for ConfigHandler {
     fn default() -> Self {
+        //TODO this should be read from conf file
+        let interface_name = "vEthernet (Default Switch)";
+
         let mut sys = System::new_all();
         sys.refresh_all();
 
         let mut templates_loc = "./src/templates".to_string();
         let mut address = "127.0.0.1".to_string();
+        let networks = sys.networks();
+
+        let mac_address = networks.iter()
+            .filter(|(&ref name, &ref data)| name.eq(interface_name))
+            .map(|(&ref name, &ref data)| data.mac_address().to_string())
+            .collect();
+        info!("Mac address: {}", mac_address);
+
         match sys.name() {
             Some(x) => {
                 if x.eq("Raspberry Pi") {
@@ -28,8 +52,10 @@ impl Default for ConfigHandler {
         ConfigHandler {
             ip_address: address,
             platform: sys.name().expect("Couldn't get OS name."),
-            templates_loc
-        } 
+            templates_loc,
+            mac_address,
+            uuid: uuid::Uuid::new_v4().to_string(),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ mod not_found;
 mod config_handler;
 
 use std::env;
-use actix_web::{get, HttpServer, App, web, Responder, post};
+use actix_web::{get, HttpServer, App, web, Responder, post, HttpResponse};
 use actix_web::error::JsonPayloadError;
+use actix_web::http::StatusCode;
+use actix_web::middleware::ErrorHandlerResponse::Response;
+use actix_web::middleware::Logger;
+use env_logger::Env;
 use handlebars::Handlebars;
+use log::info;
 use serde::Serialize;
 use crate::config_handler::ConfigHandler;
 
@@ -13,50 +18,45 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {}!\n", name)
 }
 
-#[get("/status")]
+#[get("/status/health")]
 async fn health() -> impl Responder {
-    let health = Health {
-        status: "Ok".to_string(),
-    };
-    Ok::<web::Json<Health>, JsonPayloadError>(web::Json(health))
+    // let health = Health {
+    //     status: "Ok".to_string(),
+    // };
+    // Ok::<web::Json<Health>, JsonPayloadError>(web::Json(health))
+    HttpResponse::new(StatusCode::NO_CONTENT)
 }
 
-enum Status {
-    OK,
-    NotOk
-}
-
-#[derive(Serialize)]
-struct Health {
-    status: String
-}
-
-#[get("/status/system")]
+#[get("/status/config")]
 async fn system() -> impl Responder{
     let config = ConfigHandler::default();
+    info!("Fetched config: {}", config.to_string());
     Ok::<web::Json<ConfigHandler>,JsonPayloadError>(web::Json(config))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env::set_var("RUST_LOG", "config_handler=info, main=info ,actix_web=debug,actix_server=info");
 
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     // let paths = fs::read_dir("./").unwrap();
     //
     // for path in paths {
     //     println!("Name: {}", path.unwrap().path().display())
     // }
+    info!("Starting");
 
     let mut conf = ConfigHandler::default();
 
     let mut handlebars = Handlebars::new();
-    handlebars.register_templates_directory(".html", "./src/templates/").expect("Couldn't load templates");
+    handlebars.register_templates_directory(".html", conf.templates_loc).expect("Couldn't load templates");
 
     let data = web::Data::new(handlebars);
 
     HttpServer::new(move || {
         App::new()
             .app_data(data.clone())
+            .wrap(Logger::default())
             .service(greet)
             .service(health)
             .service(system)
